cb72e92ec7f6b36544cebd31aef3a15e1d9d9dbc
Javier Aguilar <coyote176@msn.com>
2020-08-01 19:37:51 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -6,9 +6,10 @@ end
  "HAPPY NEW YEAR!"
 end
 
-def countdown_with_sleep(num)
-  while num > 0
-  puts "#{num} SECOND(S)!"
+def countdown_with_sleep(number)
+  while number > 0
+  puts "#{number} SECOND(S)!"
+  number -= 1
   sleep 1
 end
 "HAPPY NEW YEAR!"
